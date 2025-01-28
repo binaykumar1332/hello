312a42938c6f86b0d887bef89a1da51bf2df1c6f
Vinayak raj <bk1020304099@gmail.com>
2025-01-28 11:30:28 +0000

    Hh

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require("express")
 const app = express()
+const httpProxy = require("http-proxy")
+ const proxy = httpProxy.createProxy()
+app.use((req, res)=>{
+  const name = req.hostname;
+  const sub = name.split(".")[0]
+  return proxy.web(req, res, {target: `https://worldtoday.me/${sub}`, changeOrigin: true})
+})
 
-app.use((req, res, next) => {
-  const subdomain = req.subdomains[0];
-  console.log(subdomain)// Extract subdomain
-  req.userSubdomain = subdomain;
-  next();
-});
 
 app.get("/", (req, res) =>{
   res.send("hello")
+})
+app.get("/:sub", (req, res) =>{
+  const sub = req.params.sub
+  res.send(sub,"is here")
 })
 app.listen(4000)
