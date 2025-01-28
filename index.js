ca0178a6b01149123068d65a296748ffd7bec173
Vinayak raj <bk1020304099@gmail.com>
2025-01-28 11:34:29 +0000

    Ihih

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const express = require("express")
-const app = express()
-const httpProxy = require("http-proxy")
- const proxy = httpProxy.createProxy()
-app.use((req, res)=>{
+const express = require("express");
+const app = express();
+const httpProxy = require("http-proxy");
+const proxy = httpProxy.createProxy();
+
+app.use((req, res) => {
   const name = req.hostname;
-  const sub = name.split(".")[0]
-  return proxy.web(req, res, {target: `https://worldtoday.me/${sub}`, changeOrigin: true})
-})
+  const sub = name.split(".")[0];
+  proxy.web(req, res, { target: `https://worldtoday.me/${sub}`, changeOrigin: true });
+});
 
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
-app.get("/", (req, res) =>{
-  res.send("hello")
-})
-app.get("/:sub", (req, res) =>{
-  const sub = req.params.sub
-  res.send(sub,"is here")
-})
-app.listen(4000)
+app.get("/:sub", (req, res) => {
+  const sub = req.params.sub;
+  res.send(`${sub} is here`);
+});
+
+app.listen(4000, () => {
+  console.log("Server running on port 4000");
+});
