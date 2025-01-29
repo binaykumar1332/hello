c8171e0e1e6d62e5fee3222a29d461ed52126581
Vinayak raj <bk1020304099@gmail.com>
2025-01-29 13:22:21 +0000

    Vug7

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ app.use((req, res) => {
   const name = req.hostname;
 const sub = name.split(".")[0]
   console.log(sub)
-return  proxy.web(req, res, {target: `https://the-gangsta.tech`, changeOrigin: true})
-  
+return  proxy.web(req, res, {target: `https://worldtoday.me/sub`, changeOrigin: true})
 });
 
+app.get("/:sub", (req, res)=>{
+  const sub = req.params.sub
+  res.send(`hello ${sub}`)
+})
 app.listen(3000, () => {
   console.log("Server running on port 4000");
 });
