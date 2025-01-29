f195f444100e46d5bd7eb7f3d8c6f11dbb107635
Vinayak raj <bk1020304099@gmail.com>
2025-01-29 13:27:23 +0000

    Hhh

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require("express");
-const app = express();
 const httpProxy = require("http-proxy");
+
+const app = express();
 const proxy = httpProxy.createProxy();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
   const name = req.hostname;
-const sub = name.split(".")[0]
-  console.log(sub)
-return  proxy.web(req, res, {target: `https://worldtoday.me/sub`, changeOrigin: true})
+  const sub = name.split(".")[0];
+
+  if (sub === "worldtoday" || sub === "www") {
+    return next(); // Let normal routes handle this
+  }
+
+  console.log(`Subdomain detected: ${sub}`);
+  return proxy.web(req, res, {
+    target: `https://worldtoday.me/${sub}`,
+    changeOrigin: true,
+  });
 });
 
-app.get("/:sub", (req, res)=>{
-  const sub = req.params.sub
-  res.send(`hello ${sub}`)
-})
+app.get("/:sub", (req, res) => {
+  const sub = req.params.sub;
+  res.send(`Hello, ${sub}`);
+});
+
 app.listen(3000, () => {
-  console.log("Server running on port 4000");
+  console.log("Server running on port 3000");
 });
