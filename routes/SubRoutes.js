dbb85dc409ca8c8496fae73f92fd2305e02ed05d
Vinayak raj <bk1020304099@gmail.com>
2025-01-30 16:33:24 +0000

    Uvvuuv

--- routes/SubRoutes.js
+++ routes/SubRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/hello", (req, res) => {
+  console.log(req)
   const subdomain= req.subdomain;
   res.send(`Hello from ${subdomain}!`)
 })
