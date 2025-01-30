a09acc0fcd46a1adad31d64efbf30e1cba7cf512
Vinayak raj <bk1020304099@gmail.com>
2025-01-30 16:50:13 +0000

    Ihhihii

--- routes/SubRoutes.js
+++ routes/SubRoutes.js
@@ -4,7 +4,8 @@ const router = express.Router();
 router.get("/hello", (req, res) => {
   console.log("sub", req.baseUrl)
   const baseUrl = req.baseUrl;
-  const subdomain = baseUrl.split("/")[1]
+  console.log(baseUrl)
+  const subdomain = baseUrl.split("/")[0]
   
   res.send(`Hello from ${subdomain}!`)
 })
