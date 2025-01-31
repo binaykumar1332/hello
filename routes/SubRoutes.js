a20d8669f251bef7964d26f648ff485ae5e66ea0
CODER SHARMA <34667338-thegangstaguy00@users.noreply.replit.com>
2025-01-31 14:16:59 +0000

    Hhhfdf

--- routes/SubRoutes.js
+++ routes/SubRoutes.js
@@ -9,4 +9,13 @@ router.get("/hello", (req, res) => {
   
   res.send(`Hello from ${subdomain}!`)
 })
+
+router.get("/*", (req, res) => {
+  console.log("sub", req.baseUrl)
+  const baseUrl = req.baseUrl;
+  console.log(baseUrl)
+  const subdomain = baseUrl.split("/")[2]
+  
+  res.send("404 NOT FOUND")
+})
 export default router
